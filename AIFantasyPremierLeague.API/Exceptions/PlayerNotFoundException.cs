76087868e31a9e4d1ec1951e4c6d12ab5a84a0c7
mikesmith-createfuture <mike.smith@xdesign.com>
2025-11-11 14:33:23 +0000

    formatting

--- AIFantasyPremierLeague.API/Exceptions/PlayerNotFoundException.cs
+++ AIFantasyPremierLeague.API/Exceptions/PlayerNotFoundException.cs
@@ -1,7 +1,7 @@
 public class PlayerNotFoundException : Exception
 {
-   public PlayerNotFoundException(string playerId) 
-        : base($"Player with ID '{playerId}' was not found")
+    public PlayerNotFoundException(string playerId)
+         : base($"Player with ID '{playerId}' was not found")
     {
     }
 }
--- AIFantasyPremierLeague.API/Exceptions/TeamNotFoundFoundException.cs
+++ AIFantasyPremierLeague.API/Exceptions/TeamNotFoundFoundException.cs
@@ -1,7 +1,7 @@
 public class TeamNotFoundException : Exception
 {
-   public TeamNotFoundException(string teamId) 
-        : base($"Team with ID '{teamId}' was not found")
+    public TeamNotFoundException(string teamId)
+         : base($"Team with ID '{teamId}' was not found")
     {
     }
 }
